216a5d67a0ecd5bee568a9bdd1d087738b576db9
root <root@localhost.localdomain>
2019-01-17 06:49:15 -0800

    (head) se agrego BD Mysql

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/19/a0391ce6e71900191d39f64ca078a69f
@@ -0,0 +1,25 @@
+package common.System.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+ 
+ 
+@Controller
+@RequestMapping("/")
+public class AppController {
+ 
+    @RequestMapping(value = { "/"}, method = RequestMethod.GET)
+    public String homePage(ModelMap model) {
+        return "home";
+    }
+ 
+
+ 
+    @RequestMapping(value = { "/hello"}, method = RequestMethod.GET)
+    public String contactUsPage(ModelMap model) {
+        return "hello";
+    }
+}
--- gama.com/src/main/java/common/System/controller/AppController.java
+++ gama.com/src/main/java/common/System/controller/AppController.java
@@ -1,11 +1,16 @@
 package common.System.controller;
 
+import javax.servlet.ServletContextEvent;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
- 
+
+import com.sun.xml.rpc.processor.modeler.j2ee.xml.string;
+
+import common.System.configuration.AppContextListener;
+import  infrastructure.System.Adapters.*;
  
 @Controller
 @RequestMapping("/")
@@ -13,8 +18,26 @@ public class AppController {
  
     @RequestMapping(value = { "/"}, method = RequestMethod.GET)
     public String homePage(ModelMap model) {
+    	
+    	ServletContextEvent obj = new ServletContextEvent(null);
+    	obj.getServletContext();
+    	obj.getSource();
+    	AppContextListener  clase= new AppContextListener();
+    clase.name("url");
+    	
+    	String host="localhost"; 
+    	String port="3306"; 
+    	String database="Gamachicas"; 
+    	String user="root"; 
+    	String password="1988deza"; 
+    	
+    	MySqlAdapter ob = new MySqlAdapter();
+    	//javaMySQLBasic.connectDatabase("localhost.localdomain", "3306", "Gamachicas","gamachicas", "1988deza");
+    	ob.connectDatabase(host, port, database, user, password);
+    	
         return "home";
     }
+    
  
 
  
